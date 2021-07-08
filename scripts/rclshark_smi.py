ed417a1330a4809662dc4d5bb321e999ff9a80bb
Ar-Ray-code <ray255ar@gmail.com>
2021-07-08 15:13:25 +0900

    update rclshark-smi

--- scripts/rclshark_smi.py
+++ scripts/rclshark_smi.py
@@ -18,11 +18,6 @@ version_minor = 0
 version_revision = 0
 version_build = str(version_mejor) + "." + str(version_minor) + "." + str(version_revision)
 
-global_data = list()
-srv_class_list = list()
-ip_list = list()
-
-
 # ==========================================================
 
 # Call one service
@@ -58,19 +53,22 @@ class srv_main:
     
     def using_srv_fnc(self):
         if(self.ros_class.reset_client()):
-            return 1
+            return -1
         self.req = self.ros_class.get_by_service()
         if rclpy.ok():
             rclpy.spin_once(self.ros_class)
             if self.ros_class.future.done():
                 try:
-                    global_data.append(self.ros_class.future.result().callback_status)
                     rclpy.spin_once(self.ros_class)
                 except Exception:
-                    return 1
-        return 0
+                    return -1
+        return self.ros_class.future.result().callback_status
 
-def show_data():
+# ============================-===========================
+srv_class_list = list()
+ip_list = list()
+
+def show_data(input_data:list):
     print("+----------------------------------------------------------------------------+")
     print("| RCLSHARK-SMI " + version_build + "\t" + "ROS-DISTRO " + os.environ['ROS_DISTRO'] + "\t\t" + datetime.datetime.now().isoformat(timespec='seconds') + "\t     |")
     print("|============================================================================|")
@@ -78,11 +76,11 @@ def show_data():
     print("|============================================================================|")
     terminal_col = 5
 
-    for data in global_data:
+    for data in input_data:
         ip_data = int(data.ip_address.split(".")[3])
         data.local_tag = ip_data
 
-    for data in sorted(global_data, reverse=False, key=lambda x: x.local_tag):
+    for data in sorted(input_data, reverse=False, key=lambda x: x.local_tag):
         print_status = "| "+ data.ip_address + "\t\t" + str(data.cpu_percent).rjust(5) + "\t" + str(data.core_temp).rjust(5) + "\t" + str(data.mem_percent).rjust(5) + "\t" + str(data.disk_percent).rjust(5) + "\t" + str(data.process_count).rjust(5) + "\t\t" + "     |"
         print(print_status)
         terminal_col = terminal_col + 1
@@ -95,14 +93,16 @@ def show_data():
 
 def get_from_srv(_srv_list:list, _ip_list:list):
     # get data from srv_list
+    data_recieve = list()
     for i in range(len(_srv_list)):
         try:
             out = _srv_list[i].using_srv_fnc()
-            if(out):
+            if(out == -1):
                 _ip_list.remove(_srv_list[i].ip)
+            data_recieve.append(out)
         except:
             pass
-    return _srv_list, _ip_list
+    return _ip_list, data_recieve
 
 def get_ip_list() -> list:
     try:
@@ -111,8 +111,8 @@ def get_ip_list() -> list:
         return []
 
 def loop():
-    global ip_list
     global srv_class_list
+    global ip_list
     
     ip_list = get_ip_list()
         
@@ -122,10 +122,8 @@ def loop():
         for i in ip_list:
             srv_class_list.append(srv_main(i))
 
-    srv_class_list, ip_list = get_from_srv(srv_class_list, ip_list)
-
-    show_data()
-    global_data.clear()
+    ip_list, data = get_from_srv(srv_class_list, ip_list)
+    show_data(data)
 
 def input_timeout(timeout=10):
     (ready, _, _) = select.select([sys.stdin], [], [], timeout)
@@ -134,27 +132,25 @@ def input_timeout(timeout=10):
     else:
         return ''
 
+
 def ros_main(args = None):
     global ip_list
     global srv_list
     rclpy.init(args=args)
 
     while rclpy.ok():
-        
         t = threading.Thread(target=loop,args=())
         t.setDaemon(True)
         t.start()
         t.join(timeout=5.0)
 
         if t.is_alive():
-            ip_list.clear()
-            for srv in srv_class_list:
-                del srv
             print("Timeout Error")
             exit(1)
 
         if input_timeout(0.1) == 'q':
             print("quit")
+            srv_class_list.clear()
             exit(0)
 
         del t
